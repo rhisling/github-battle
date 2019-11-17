8941e13d6aefb6728863129daf7aa984514c9323
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-11-16 20:54:17 -0800

    Adding reset

--- src/containers/Battle.js
+++ src/containers/Battle.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Instruction from '../components/Instruction';
-import PlayerInput from '../components/PlayerInput';
-import './Battle.css';
-import PlayerPreview from '../components/PlayerPreview';
-import Results from './Results';
+import React from "react";
+import Instruction from "../components/Instruction";
+import PlayerInput from "../components/PlayerInput";
+import "./Battle.css";
+import PlayerPreview from "../components/PlayerPreview";
+import Results from "./Results";
 
 class Battle extends React.Component {
   state = {
@@ -26,43 +26,56 @@ class Battle extends React.Component {
   render() {
     const { playerOne, playerTwo, battle } = this.state;
     if (battle) {
-      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+      return (
+        <Results
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+          onReset={() =>
+            this.setState({
+              playerOne: null,
+              playerTwo: null,
+              battle: false
+            })
+          }
+        />
+      );
     }
     return (
       <>
-        <Instruction></Instruction>
+        <Instruction />
         <div className="players-container">
           <h1 className="center-text header-lg">Players</h1>
           <div className="row space-around">
             {playerOne === null ? (
               <PlayerInput
                 label="Player One"
-                onSubmit={player => this.handleSubmit('playerOne', player)}
+                onSubmit={player => this.handleSubmit("playerOne", player)}
               />
             ) : (
               <PlayerPreview
                 username={playerOne}
                 label="Player One"
-                onReset={() => this.handleReset('playerOne')}
+                onReset={() => this.handleReset("playerOne")}
               />
             )}
             {playerTwo === null ? (
               <PlayerInput
                 label="Player Two"
-                onSubmit={player => this.handleSubmit('playerTwo', player)}
+                onSubmit={player => this.handleSubmit("playerTwo", player)}
               />
             ) : (
               <PlayerPreview
                 username={playerTwo}
                 label="Player Two"
-                onReset={() => this.handleReset('playerTwo')}
+                onReset={() => this.handleReset("playerTwo")}
               />
             )}
           </div>
           {playerOne && playerTwo && (
             <button
               className="btn btn-dark btn-space"
-              onClick={() => this.setState({ battle: true })}>
+              onClick={() => this.setState({ battle: true })}
+            >
               Battle
             </button>
           )}
--- src/containers/Results.js
+++ src/containers/Results.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import { battle } from '../utils/api';
-import { css } from 'emotion';
-import {
-  FaCompass,
-  FaBriefcase,
-  FaUsers,
-  FaUserFriends,
-  FaCode,
-  FaUser
-} from 'react-icons/fa';
-import Card from '../components/Card';
+import React from "react";
+import { battle } from "../utils/api";
+import { css } from "emotion";
+import Card from "../components/Card";
+import ProfileList from "../components/ProfileList";
+import PropTypes from "prop-types";
+
+const styles = {
+  container: {
+    position: "relative",
+    display: "flex"
+  },
+  tooltip: {
+    boxSizing: "border-box",
+    position: "absolute",
+    width: "160px",
+    bottom: "100%",
+    left: "50%",
+    marginLeft: "-80px",
+    borderRadius: "3px",
+    backgroundColor: "hsla(0, 0%, 20%, 0.9)",
+    padding: "7px",
+    marginBottom: "5px",
+    color: "#fff",
+    textAlign: "center",
+    fontSize: "14px"
+  }
+};
 
 class Results extends React.Component {
   state = {
@@ -50,83 +66,47 @@ class Results extends React.Component {
             color: #ff1616;
             font-size: 20px;
             margin: 50px 0;
-          `}>
+          `}
+        >
           {error}
         </p>
       );
     }
     return (
-      <div className="grid space-around container-sm">
-        <Card
-          header={winner.score === loser.score ? 'Tie' : 'Winner'}
-          subheader={`Score: ${winner.score.toLocaleString()}`}
-          avatar={winner.profile.avatar_url}
-          href={winner.profile.html_url}
-          name={winner.profile.login}>
-          <ul className="card-list">
-            <li>
-              <FaUser color="rgb(239, 115, 115)" size={22} />
-              {winner.profile.name}
-            </li>
-            {winner.profile.location && (
-              <li>
-                <FaCompass color="rgb(144,115,255)" size={22} />
-                {winner.profile.location}
-              </li>
-            )}
-            {winner.profile.company && (
-              <li>
-                <FaBriefcase color="#794458" size={22} />
-                {winner.profile.location}
-              </li>
-            )}
-            <li>
-              <FaUsers color="rgb(129, 195, 245)" size={22} />
-              {winner.profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-              <FaUserFriends color="rgb(64, 183, 95)" size={22} />
-              {winner.profile.followers.toLocaleString()} following
-            </li>
-          </ul>
-        </Card>
+      <>
+        <div className="grid space-around container-sm">
+          <Card
+            header={winner.score === loser.score ? "Tie" : "Winner"}
+            subheader={`Score: ${winner.score.toLocaleString()}`}
+            avatar={winner.profile.avatar_url}
+            href={winner.profile.html_url}
+            name={winner.profile.login}
+          >
+            <ProfileList profile={winner.profile} />
+          </Card>
 
-        <Card
-          header={winner.score === loser.score ? 'Tie' : 'Loser'}
-          subheader={`Score: ${loser.score.toLocaleString()}`}
-          avatar={loser.profile.avatar_url}
-          name={loser.profile.login}
-          href={loser.profile.html_url}>
-          <ul className="card-list">
-            <li>
-              <FaUser color="rgb(239, 115, 115)" size={22} />
-              {loser.profile.name}
-            </li>
-            {loser.profile.location && (
-              <li>
-                <FaCompass color="rgb(144,115,255)" size={22} />
-                {loser.profile.location}
-              </li>
-            )}
-            {loser.profile.company && (
-              <li>
-                <FaBriefcase color="#794458" size={22} />
-                {loser.profile.location}
-              </li>
-            )}
-            <li>
-              <FaUsers color="rgb(129, 195, 245)" size={22} />
-              {loser.profile.followers.toLocaleString()} followers
-            </li>
-            <li>
-              <FaUserFriends color="rgb(64, 183, 95)" size={22} />
-              {loser.profile.followers.toLocaleString()} following
-            </li>
-          </ul>
-        </Card>
-      </div>
+          <Card
+            header={winner.score === loser.score ? "Tie" : "Loser"}
+            subheader={`Score: ${loser.score.toLocaleString()}`}
+            avatar={loser.profile.avatar_url}
+            name={loser.profile.login}
+            href={loser.profile.html_url}
+          >
+            <ProfileList profile={loser.profile} />
+          </Card>
+        </div>
+        <button className="btn btn-dark btn-space" onClick={this.props.onReset}>
+          Reset
+        </button>
+      </>
     );
   }
 }
+
+Results.propTypes = {
+  playerOne: PropTypes.string.isRequired,
+  playerTwo: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+};
 
 export default Results;
