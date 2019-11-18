277db85476914d351e116d33016e0366ece711a5
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-11-17 20:13:44 -0800

    Adding lazy loading using lazy and suspense api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Popular from './containers/Popular';
-import Battle from './containers/Battle';
 import { ThemeProvider } from './contexts/Theme';
 import Nav from './components/Nav';
 import { Router } from '@reach/router';
-import Results from './containers/Results';
+import NotFound from './components/NotFound';
+import Loading from './components/Loading';
+
+const Popular = React.lazy(() => import('./containers/Popular'));
+const Battle = React.lazy(() => import('./containers/Battle'));
+const Results = React.lazy(() => import('./containers/Results'));
 
 const App = () => {
   const [theme, toggleTheme] = useState('light');
@@ -16,11 +19,14 @@ const App = () => {
         <div className={theme}>
           <div className="container">
             <Nav />
-            <Router>
-              <Popular path="/" />
-              <Battle path="/battle" />
-              <Results path="/battle/results" />
-            </Router>
+            <React.Suspense fallback={<Loading text="Loading" speed={300} />}>
+              <Router>
+                <Popular path="/" />
+                <Battle path="/battle" />
+                <Results path="/battle/results" />
+                <NotFound default />
+              </Router>
+            </React.Suspense>
           </div>
         </div>
       </ThemeProvider>
